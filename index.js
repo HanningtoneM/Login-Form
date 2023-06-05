919fa64f4b1d8379c0d01d5b70e08b39f85a4236
HanningtoneM <hanningtonepompyy@gmail.com>
2023-06-05 17:23:44 +0300

    Function to manupliate array

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const labels = document.querySelectorAll('.form-control label')
 labels.forEach( label => {
     label.innerHtml = label.innerText
         .split('')
-        .map((letter, idx) => ``)
+        .map((letter, idx) => `<span>${letter}</span>`)
+        .join('')
 })
